06a289bd7525c358bc50b31f5be40058c28a62f2
duplessisk <klduplessis@gmail.com>
2021-02-09 18:08:36 -0800

    m

--- client_side_code/html_page_elements/checkboxes.js
+++ client_side_code/html_page_elements/checkboxes.js
@@ -23,7 +23,7 @@ for (var i = 0; i < 10; i++) {
             "</div>" + 
                 "</div>" +
                 "<div class= 'cell-image-row-container'>" + 
-                    "<img src='/static/object_images/object" + imageNum + i +".png' alt='This image was originally intended to display a row of cell images' id='cellImage'>" + 
+                    "<img src='/static/final_object_images/object" + imageNum + i +".png' alt='This image was originally intended to display a row of cell images' id='cellImage'>" + 
                 "</div>" + 
             "</div>" + 
         "</div>"
