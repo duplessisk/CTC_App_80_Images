7406a9d355a2c209d5aff0200b89f6895ba98a7e
duplessisk <klduplessis@gmail.com>
2021-02-10 07:43:29 -0800

    minor changes

---
+++ client_side_code/html_page_elements/buttons/next_btn_instructions_page.js
@@ -0,0 +1,14 @@
+// Creates the Begin button on the welcome and instructions pages.
+
+var nextBtnInstructionsPageDiv = document.createElement('div');
+nextBtnInstructionsPageDiv.id = "nextBtnInstructionsPageDiv";
+document.querySelector("#form").appendChild(nextBtnInstructionsPageDiv);
+
+var nextBtnInstructionsPage = document.createElement('input');
+nextBtnInstructionsPage.type = "submit";
+nextBtnInstructionsPage.className = "btn";
+nextBtnInstructionsPage.id = "nextBtnInstructionsPage";
+nextBtnInstructionsPage.name = "btn";
+nextBtnInstructionsPage.value = "Next";
+document.querySelector("#nextBtnInstructionsPageDiv")
+    .appendChild(nextBtnInstructionsPage);
